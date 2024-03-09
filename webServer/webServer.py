dc98138af718bf131b5ceb2c710603a1d5542ad2
pexas <josebernardo@tecnico.ulisboa.pt>
2024-03-09 13:08:42 +0000

    Implemented analytics page to show graphs and add_footage endpoint

--- webServer/webServer.py
+++ webServer/webServer.py
@@ -3,6 +3,11 @@ from functools import wraps
 import secrets
 import random
 
+import plotly.graph_objs as go
+
+import time
+from datetime import datetime
+
 app = Flask(__name__)
 
 # Placeholder user credentials (replace with secure storage)
@@ -28,14 +33,14 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+
         if username in users and users[username] == password:
             # Successful login (replace with redirection to protected area)
             session['logged_in'] = True
             return redirect('/')
         else:
             error = 'Invalid Credentials. Please try again.'
-    
+
     return render_template('login.html', error=error)
 
 @app.route('/logout', methods=['POST'])
@@ -51,18 +56,71 @@ def get_pin():
     pin = random.randint(1000, 9999)
 
     # TODO: Store the pin in the DB with the timestamp
-    return render_template('getpin.html', pin=pin)   
-    
+    return render_template('getpin.html', pin=pin)
+
+@app.route('/analytics', methods=['POST'])
+#@login_required
+def analytics():
+    # replace with database retrieval
+    humidity_values = [30, 35, 40, 60, 70, 40, 20]
+    temperature_values = [30, 32, 36, 40, 38, 36, 35]
+    timestamps = [
+        datetime(2024, 3, 8, 9, 0),  # 9:00 AM
+        datetime(2024, 3, 8, 10, 0),  # 10:00 AM
+        datetime(2024, 3, 8, 11, 0),  # 11:00 AM
+        datetime(2024, 3, 8, 12, 0),  # 12:00 PM
+        datetime(2024, 3, 8, 13, 0),  # 1:00 PM
+        datetime(2024, 3, 8, 14, 0),  # 2:00 PM
+        datetime(2024, 3, 8, 15, 0),  # 3:00 PM
+    ]
+
+    # Create temperature graph
+    trace = go.Scatter(x=timestamps, y=temperature_values, mode='lines+markers', name='Humidity')
+    layout = go.Layout(title='Humidity Over Time',
+                       xaxis=dict(title='Time'),
+                       yaxis=dict(title='Humidity (%)'))
+    fig = go.Figure(data=[trace], layout=layout)
+    temperature_graph_json = fig.to_json()
+
+    # Create humidity graph
+    trace = go.Scatter(x=timestamps, y=humidity_values, mode='lines+markers', name='Humidity')
+    layout = go.Layout(title='Humidity Over Time',
+                       xaxis=dict(title='Time'),
+                       yaxis=dict(title='Humidity (%)'))
+    fig = go.Figure(data=[trace], layout=layout)
+    humidity_graph_json = fig.to_json()
+
+    return render_template('analytics.html', data={'humidity': humidity_graph_json, 'temperature': temperature_graph_json})
+
+@app.route('/add_footage', methods=['POST'])
+def add_footage():
+    media = request.data
+
+    filename = 'media-' + str(time.time()) + '.mp3'
+
+    with open(filename, 'wb') as f:
+        f.write(media)
+
+    return 'Footage added'
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
-def main():    
-    
+def main():
+
   #TODO fetch values from DB  
+  # Qualify light
+  light_value = 250
+  if light_value < 50:
+      light = 'dark'
+  elif light_value < 500:
+      light = 'dim'
+  else:
+      light = 'bright'
+
   data = {
-    "temperature": "13C",
-    "humidity": "20%",
-    "luminosity": "Dark",
+    "temperature": 13,
+    "humidity": 20,
+    "luminosity": light,
     "waterLeaks": "No leaks detected",
   }
 
