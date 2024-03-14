e078c1d514c8f8df26182eff86b664894386e5c6
pexas <josebernardo@tecnico.ulisboa.pt>
2024-03-14 17:39:53 +0000

    add raspberry to add measurements by requesting to the server

--- raspberry-modules/serial_reader.py
+++ raspberry-modules/serial_reader.py
@@ -1,9 +1,13 @@
 import serial
 import os
+import sys
+import requests
 
-#serial_port = os.getenv("SERIAL_PORT")
-serial_port = "/dev/tty.usbmodem101"
+serial_port = os.getenv("SERIAL_PORT")
+server_url = os.getenv("SERVER_URL") or "127.0.0.1:5001"
 baud_rate = 9600
+
+print(f"Server URL: {server_url}")
 
 # Initialize the serial connection
 ser = serial.Serial(serial_port, baud_rate)
@@ -12,7 +16,20 @@ try:
     while True:
         # Read a line from the serial port
         line = ser.readline().decode("utf-8").strip()
-        print(line)
+        info = line.split()
+
+        if info[0] == "warning":
+            print(line)
+            #request.post('http://' + server_url + '/notify')
+        elif info[0] == "measurement":
+            print(info)
+            requests.post('http://' + server_url + '/add-measure', data={'sensorid': info[1], 'value': info[2]})
+        elif info[0] == "error":
+            print(line)
+        else:
+            raise ValueError("This should never happen!")
+
 except KeyboardInterrupt:
     print("Exiting...")
+    conn.close() # Close the database connection when the program exits
     ser.close()  # Close the serial connection when the program exits
